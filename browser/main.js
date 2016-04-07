1709a284dd06b2be55f3e1ef0be8f184938c7d9f
Sreejitha Vijayan <sreejitha.vijayan@gmail.com>
2016-04-06 21:56:20 -0700

    working on new side bar

--- browser/main.js
+++ browser/main.js
@@ -1,8 +1,10 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const StyleSheet = require('react-style')
 const SplitPane = require('react-split-pane');
 const PartsView = require('./PartsView');
 const NetworkView = require('./NetworkView');
+const Menu = require('./menu');
 
 import './main.css'
 
@@ -11,15 +13,15 @@ window.onload = () => {
   $('#root').w2layout({
       name: 'layout',
       panels: [
-          { type: 'top', size: 50, resizable: true, style: pstyle, content: 'top' },
-          { type: 'left', size: 200, resizable: true, style: pstyle, content: 'left' },
+          { type: 'top', size: 50, resizable: true, hidden: true, style: pstyle, content: 'top' },
+          { type: 'left', size: 200, resizable: true, hidden: true, style: pstyle, content: 'left' },
           { type: 'main', style: 'background-color: white;', overflow: 'hidden' },
-          { type: 'preview', size: '50%', resizable: true, hidden: true, style: pstyle, content: 'preview' },
-          { type: 'right', size: 200, resizable: true, style: pstyle, content: 'right' },
+          { type: 'preview', size: '50%', resizable: true, hidden: false, style: pstyle, content: 'preview' },
+          { type: 'right', size: 200, resizable: true, hidden: true, style: pstyle, content: 'right' },
           { type: 'bottom', size: 50, resizable: true, hidden: true, style: pstyle, content: 'bottom' }
       ]
   });
-  var pstyle = 'background-color: #F0F0C1; border: 1px solid #dfdfdf; padding: 5px;';
+/*  var pstyle = 'background-color: #F0F0C1; border: 1px solid #dfdfdf; padding: 5px;';
   $().w2layout({
       name: 'layout2',
       panels: [
@@ -30,17 +32,26 @@ window.onload = () => {
           { type: 'right', size: 80, resizable: true, style: pstyle, content: 'right' },
           { type: 'bottom', size: 40, resizable: true, hidden: true, style: pstyle, content: 'bottom' }
       ]
-  });
+  });*/
 
   w2ui['layout'].content('main', document.getElementById('root2'));
+  let menuStyle = StyleSheet.create({
+  container: {
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#d6d7da',
+  },
+  title: {
+    fontSize: 19,
+    fontWeight: 'bold',
+  },
+  activeTitle: {
+    color: 'red',
+  },
+});
   ReactDOM.render(
-    <SplitPane split="vertical" minSize="50">
-        <div>
-        </div>
-        <SplitPane split="horizontal">
-            <div>Hello2</div>
-        </SplitPane>
-    </SplitPane>,
+      <Menu/>
+    ,
     document.getElementById('root2')
   );
 };
