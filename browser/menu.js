2da36d55ed29b00595ddc1b195652d5607c16607
Deepak Chandran <dchandran1@gmail.com>
2016-04-09 23:28:10 -0700

    file open button

--- browser/menu.js
+++ browser/menu.js
@@ -1,11 +1,15 @@
 const BurgerMenu = require('react-burger-menu').slide;
+const Dropzone = require('react-dropzone');
+
+require('./images/open_file.png');
+require('./images/map.png');
 require('./images/movie.png');
 require('./images/python.png');
 require('./styles/menu.css');
 
 class Menu extends React.Component {
   render() {
-    let styles = {
+    let menuStyles = {
       bmBurgerButton: {
         position: 'fixed',
         width: '2em',
@@ -40,15 +44,47 @@ class Menu extends React.Component {
       }
     }
 
+    let menuItems = [
+      {
+        class: 'mapButton',
+        tooltip: 'Overall concept design'
+      },
+      {
+        class: 'animationButton',
+        tooltip: 'Animate the simulated results'
+      },
+      {
+        class: 'pythonButton',
+        tooltip: 'Simulate using Python code'
+      }
+    ];
+
     let myFunc = () => {
       debugger;
     };
 
-    return (<BurgerMenu styles={styles} width={150}>
-      <div className="tooltip" data-hint="HELLO">
-      <input type="button" onClick={myFunc} className="menuButton animationButton tooltip"/>
-      </div>
-      <input type="button" onClick={myFunc} className="menuButton pythonButton tooltip"/>
+    let onDrop = (files) => {
+      debugger;
+    };
+
+    let dropZoneStyles = {
+      width: "2em",
+      height: "2em"
+    };
+
+    return (<BurgerMenu styles={menuStyles} width={150}>
+        <div className="tooltip" data-hint="Open a project file">
+          <Dropzone className="menuButton fileOpenButton" onDrop={onDrop}/>
+        </div>
+        {
+          menuItems.map(function(item) {
+            let classNames = "menuButton tooltip " + item.class;
+            return (
+              <div className="tooltip" data-hint={item.tooltip}>
+              <input type="button" onClick={myFunc} className={classNames}/>
+              </div>);
+          })
+        }
     </BurgerMenu>);
   }
 }
